476bfdbd45348831be28a52aa52f0bfab3727afc
luismarimom <luismarimom@gmail.com>
2021-05-02 22:46:01 -0500

    pruebas en local

--- src/main/java/co/com/choucair/banitsmo/examen/userinterface/HomePage.java
+++ src/main/java/co/com/choucair/banitsmo/examen/userinterface/HomePage.java
@@ -7,5 +7,5 @@ import net.thucydides.core.annotations.DefaultUrl;
 @DefaultUrl("https://www.latam.com/es_co/")
 public class HomePage extends PageObject {
     public static final Target LINK_MIS_VIAJES = Target.the("Link para ir al menu mis viajes")
-            .locatedBy("//nav//p[contains(text(), 'Mis viajes')]");
+            .locatedBy("//nav//*[contains(text(), 'Mis viajes')]");
 }
